f5e23a67d5d7820a4ff275b3e302bac3adbb8e34
MyrakTechgit config --global user.email <maaziechezona@gmail.com>
2026-06-23 11:35:27 +0100

    second commit

---
+++ error.py
@@ -0,0 +1,12 @@
+score = int(input("enter your score:"))
+if score >= 80:
+    print("Grade: A Execellent!")
+elif score >= 70:
+    print("Grade: B Very Good!")
+elif score >= 60:
+    print("Grade: C Good!")
+elif score >= 40:
+    print("Grade: D Fair!")
+else:
+    print("Grade: F Failed")
+    
